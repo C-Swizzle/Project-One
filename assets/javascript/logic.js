b6f50b59b179e57f7cb99c1e654b7e9f47c56771
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-02-02 10:48:24 -0800

    init

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyBUm91ISX7r3E0e4GQPHlX6vwe-GT03uWQ",
+    authDomain: "project-one-52a15.firebaseapp.com",
+    databaseURL: "https://project-one-52a15.firebaseio.com",
+    projectId: "project-one-52a15",
+    storageBucket: "project-one-52a15.appspot.com",
+    messagingSenderId: "653933644732"
+  };
+  firebase.initializeApp(config);
